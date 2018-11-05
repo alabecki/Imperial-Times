069c00ce79a167d5af4d4cdd387461a00b9267e3
Adam Labecki <adamlabecki@gmail.com>
2018-11-05 00:18:00 -0800

    Add files via upload

---
+++ Utility/Save.cs
@@ -0,0 +1,30 @@
+﻿using assemblyCsharp;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Save
+{
+
+    public Save CreateNewSave(string name)
+    {
+        Save save = new Save();
+        var folder = Directory.CreateDirectory(Application.persistentDataPath + name);
+        return save;
+    }
+
+
+
+    public void SaveGame(string name)
+    {
+        Save save = CreateNewSave(name);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+    }
+}
