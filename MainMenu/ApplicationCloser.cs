0145058a5a9307d0551cc6e79ee3eccabe3d2e5a
Adam Labecki <adamlabecki@gmail.com>
2018-11-05 00:10:56 -0800

    Add files via upload

---
+++ MainMenu/ApplicationCloser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationCloser : MonoBehaviour
+{
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
+
+	
+
